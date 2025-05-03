ba740e9edf25200f9fcfbb0d34bffd9d2f9cb02a
Itamar Medrano <martinezitamar777.com>
2025-05-02 19:12:49 -0600

    Avance
    # Conflicts:
    #	package.json
    #	pnpm-lock.yaml
    #	src/main.ts
    #	src/router/index.ts
    #	src/views/AboutView.vue
    #	src/views/EventView.vue
    #	src/views/HomeView.vue
    #	src/views/ServicesView.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- Footer -->
+  <footer class="mt-20 py-10 border-t-2 border-gray-200">
+    <div class="container mx-auto px-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 border-b border-gray-200 pb-8">
+        <div>
+          <h3 class="text-xl font-bold mb-4">Contacto y Ubicación</h3>
+          <p><strong>📍 Dirección:</strong> Parque central Los Emigrantes, Intipucá</p>
+          <p><strong>📞 Teléfono:</strong> +503 0000-0000</p>
+          <p><strong>📧 Email:</strong> rinconintipuqueno@gmail.com</p>
+        </div>
+        <div>
+          <h4 class="font-bold mb-4">Síguenos en:</h4>
+          <div class="flex space-x-6 border-t border-gray-200 pt-4">
+            <a href="https://facebook.com" aria-label="Facebook" target="blank"><img src="../../public/img/facebook.png" alt="Facebook" width="40"></a>
+            <a href="https://instagram.com" aria-label="Instagram" target="blank"><img src="../../public/img/instagram.png" alt="Instagram" width="40"></a>
+            <a href="https://wa.me" aria-label="WhatsApp" target="blank"><img src="../../public/img/whatsapp.png" alt="WhatsApp" width="40"></a>
+            <a href="https://ticktok.com" aria-label="TikTok" target="blank"><img src="../../public/img/tiktok.png" alt="TikTok" width="40"></a>
+          </div>
+        </div>
+      </div>
+      <div class="mt-8 text-center text-black">
+        <p>&copy; 2025 Restaurante Rincón Intipuqueño. Todos los derechos reservados.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import LinkNav from '@/components/LinkNav.vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header class="bg-base-600 p-5 border-b-2 border-gray-200">
+    <!-- Contenedor para centrar el contenido -->
+    <div class="max-w-7xl mx-auto flex items-center justify-between">
+      <!-- Logo y Título -->
+      <div class="flex items-center">
+        <!-- Logo del restaurante -->
+        <img src="../../public/img/logo.jpeg" alt="Logo del Restaurante Rincón Intipuqueño" class="w-24 h-16 object-cover rounded-full ">
+        <h1 class="text-3xl font-bold ml-4 text-xs sm:text-base md:text-lg lg:text-xl xl:text-2xl">{{ props.title }}</h1>
+      </div>
+      <!-- Navegación -->
+      <nav class="flex space-x-4 hidden sm:flex">
+        <link-nav text="Inicio" link="/"/>
+        <link-nav text="Eventos" link="/event"/>
+        <link-nav text="Sobre Nosotros" link="/about"/>
+        <link-nav text="Servicios" link="/services"/>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
